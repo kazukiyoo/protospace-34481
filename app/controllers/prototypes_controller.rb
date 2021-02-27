b5062e312072139b0175ca8b7f88408912ed1677
kazukiyoo <kazukiyoshibata@gmail.com>
2021-02-27 12:22:33 +0900

    updateアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,8 +29,9 @@ class PrototypesController < ApplicationController
  end
 
  def update
-  if Prototype.update(prototype_params)
-   redirect_to edit_prototype_path
+  @prototype = Prototype.find(params[:id])
+  if @prototype.update(prototype_params)
+     redirect_to root_path
   else
     render :edit
   end
